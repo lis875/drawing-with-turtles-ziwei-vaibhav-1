09289b1162ca80c42d4eb9050b3119d56704d035
Vaibhav Bakliwal <vaibhavbakliwal1998@gmail.com>
2023-10-09 13:57:59 -0500

    Exercise 9: Flag of Ghana

--- flag.py
+++ flag.py
@@ -16,14 +16,35 @@ def bar(bar_color):
     right(90)
     end_fill()
 
+def star():
+    fillcolor("black")
+    begin_fill()
+    for i in range(5):
+        forward(20)
+        left(72)
+        forward(20)
+        right(144)
+    end_fill()
+
+    # for j in range(5):
+    #     forward(50)
+    #     right(144)
+
 ### Main code that gets run, to draw the german flag
 # Do not modify code below this line for Exercise 7.  It is OK to add code below this for Exercise 8
 
-bar("black")
-right(90); forward(50); left(90)
 bar("red")
 right(90); forward(50); left(90)
 bar("yellow")
+right(90); forward(50); left(90)
+bar("green")
+forward(135)
+penup()
+left(90)
+forward(30)
+right(90)
+pendown()
+star()
 
 done()
 
